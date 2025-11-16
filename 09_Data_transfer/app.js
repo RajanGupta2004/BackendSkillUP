9158042c2bed6fcaf11e226a6827a7391bead5c1
Angad Gupta <127626872+Angad-Gupta-2005@users.noreply.github.com>
2025-11-16 23:45:18 +0530

    partiall data sharing

--- 09_Data_transfer/app.js
+++ 09_Data_transfer/app.js
@@ -24,11 +24,36 @@ app.get("/text", (req, res) => {
     highWaterMark: 64,
   });
 
-  streamData.on("data", (chunk) => {
-    console.log("sending data", chunk.toString());
-  });
+  // streamData.on("data", (chunk) => {
+  //   console.log("sending data", chunk.toString());
+  // });
 
   streamData.pipe(res);
+});
+
+app.get("/music", (req, res) => {
+  const songPath = path.join(__dirname, "song.mp3");
+  const stat = fs.statSync(songPath);
+
+  const range = req.headers.range;
+  if (!range) {
+    return res.status(400).send("Requires Range header");
+  }
+
+  const CHUNK_SIZE = 1 * 1e6; // 1MB
+  const start = Number(range.replace(/\D/g, ""));
+  const end = Math.min(start + CHUNK_SIZE, stat.size - 1);
+
+  const contentLength = end - start + 1;
+
+  res.writeHead(206, {
+    "Content-Range": `bytes ${start}-${end}/${stat.size}`,
+    "Accept-Ranges": "bytes",
+    "Content-Length": contentLength,
+    "Content-Type": "audio/mpeg",
+  });
+
+  fs.createReadStream(songPath, { start, end }).pipe(res);
 });
 
 app.listen(PORT, () => {
